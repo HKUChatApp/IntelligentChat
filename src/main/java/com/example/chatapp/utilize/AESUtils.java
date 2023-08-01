45eb31c5fedf8bd48faddc1bcb83031fcb2d7a2d
Jdream1109 <276748475@qq.com>
2023-08-01 18:50:45 +0800

    最终版本

---
+++ src/main/java/com/example/chatapp/utilize/AESUtils.java
@@ -0,0 +1,27 @@
+package com.example.chatapp.utilize;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class AESUtils {
+
+    private static final String ALGORITHM = "AES";
+    private static final String KEY = "aes_key"; // 需要确保和前端加密时使用的密钥相同
+
+    public static String encrypt(String value) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(KEY.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedBytes = cipher.doFinal(value.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public static String decrypt(String encryptedValue) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(KEY.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedValue));
+        return new String(decryptedBytes);
+    }
+}
